630ac5a050eeb4d199aec8ef4f2672acd3a56be6
Ali Khattab <aliktb@zohomail.eu>
2023-06-25 13:36:45 +0100

    adding extre homebrew packages

--- init.sh
+++ init.sh
@@ -4,4 +4,8 @@ chmod +x hooks/*
 
 cp hooks/* .git/hooks
 
+xcode-select --install
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 brew install $(cat brew-packages.txt)
