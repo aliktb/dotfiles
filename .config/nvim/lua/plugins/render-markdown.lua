98d0a38156a25b618011d09da59e36250f5f43c1
Ali Khattab <aliktb@zohomail.eu>
2025-06-16 14:58:12 +0100

    Enable and configure lazyvim markdown plugin

---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,20 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    checkbox = {
+      enabled = true,
+    },
+    code = {
+      right_pad = 1,
+      sign = true,
+      width = "block",
+    },
+    heading = {
+      border = true,
+      enabled = true,
+      icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+      position = "inline",
+      sign = true,
+    },
+  },
+}
