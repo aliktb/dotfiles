485c28c76f6c59c4afc5459fcf38e921e18c0d60
Ali Khattab <aliktb@zohomail.eu>
2025-10-12 13:32:40 +0100

    Configure ALE to use Java LSP server for Java files

--- .config/nvim/lua/plugins/ale.lua
+++ .config/nvim/lua/plugins/ale.lua
@@ -8,6 +8,7 @@ return {
     g.ale_ruby_rubocop_auto_correct_all = 1
 
     g.ale_linters = {
+      java = { "java_langauge_server" },
       ruby = { "rubocop", "ruby" },
       lua = { "lua_language_server" },
     }
