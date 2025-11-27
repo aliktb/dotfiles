1e65780475cbcd25ae2ea146cdf721d0822b0d1d
Ali Khattab <aliktb@zohomail.eu>
2025-11-27 20:38:31 +0000

    Setup autocompletions for AWS clusterawsadm tool

--- .zshrc.d/common/40-autocompletions.zsh
+++ .zshrc.d/common/40-autocompletions.zsh
@@ -1,6 +1,9 @@
 # Set Flux autocompletion
 command -v flux >/dev/null && . <(flux completion zsh)
 
+# Set clusterawsadm autocompletion
+command -v clusterawsadm >/dev/null && . <(clusterawsadm completion zsh)
+
 # AWS CLI autocompletions
 autoload -Uz compinit && compinit
 complete -C aws_completer aws
